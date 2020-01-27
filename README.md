6873bd39ab6dfacaefd10e610e3daee850bf8426
mouniryoussef <mounir.youssef@gmail.com>
2020-01-26 22:47:41 -0500

    1) update the readme file and 2) implement the managecountry method

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# RestCountriesTest
-initial version
+## Restful api Country automation test using BDD
+All this does is to show you how to install and run Cucumber scenarios to test estfull api
+There is a two feature files with one scenario outilene with 4 examples each. Each scenario has three steps to validate the basic functionality of the api. note that performance test scenarios are not covered here
+##Get the code
+#Git:
+
+git clone https://github.com/mouniryoussef/RestCountriesTest.git  
+cd RestCountriesTest
+
+#Use Maven
+Open a command window and run  
+mvn package  
+This command will compile and test the feature files and also generate the jar file  
+
+mvn test  
+This command will run the RunnerTest that validate the cucumber features : src/test/resources/com/restcountries/features/RestCountriesByCode.feature and src/test/resources/com/restcountries/features/RestCountriesByName.feature files    
+
+java -jar target/RestCountriesTest-1.0.0-SNAPSHOT.jar  
+This Command will execute the CountryApp main java class to run the country/capital application  
+
+##BBD reports
+under target/SystemTestReport you will find 3 reports in html, json and xml format  
+
+## BDD features code refactory
+the cucumber features and step definitions are yet to be completed as we still need to refactor the code and alo performance test is in the plan to be added later
--- src/main/java/main/CountryApp.java
+++ src/main/java/main/CountryApp.java
@@ -1,24 +1,31 @@
 package main;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.Country;
+import model.Message;
 import service.CountryService;
 import utilities.CountryApiEnum;
 
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class CountryApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //Choice shout be one the 3 options
         while (true) {
             Scanner scan = new Scanner(System.in);
-            System.out.println("Please enter your choice (C = Country code, N = Country name, q to Quit):");
+            System.out.println("Please enter your choice to find the capital(c = Country code, n = Country name, q to Quit):");
             String userInput = scan.nextLine();
             if (userInput.equalsIgnoreCase("q") || userInput.equalsIgnoreCase("c") || userInput.equalsIgnoreCase("n")) {
                 //Quit
                 if (userInput.equalsIgnoreCase("q")) {
-                    System.out.println("Program Ended");
+                    System.out.println("Application Ended");
                     return;
                 } else {
                     if (userInput.equalsIgnoreCase("c")) {
@@ -27,8 +34,8 @@ public class CountryApp {
                         //Input is valid
                         if (countryCode != null && countryCode.length() >= 2 && countryCode.length() <= 3) {
                             CountryService.setCountryEndPoint(CountryApiEnum.BY_CODE);
-                            Response countryByCode = CountryService.getCountryByCode(countryCode);
-                            manageResponse(countryByCode);
+                            Response countryByCodeResponse = CountryService.getCountryByCode(countryCode);
+                            manageResponse(countryByCodeResponse);
                             System.out.println("to call country endpoint by code");
                         } else {
                             System.err.println("Invalid country code!!!");
@@ -40,8 +47,8 @@ public class CountryApp {
                         String countryName = scan.next();
                         if (countryName != null) {
                             CountryService.setCountryEndPoint(CountryApiEnum.BY_NAME);
-                            Response countryByName = CountryService.getCountryByName(countryName);
-                            manageResponse(countryByName);
+                            Response countryByNameResponse = CountryService.getCountryByName(countryName);
+                            manageResponse(countryByNameResponse);
                         } else {
                             System.err.println("Invalid country name!!!");
                         }
@@ -53,8 +60,27 @@ public class CountryApp {
         }
     }
 
-    private static void manageResponse(Response response) {
+    private static void manageResponse(Response response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 
+        if ( response.getStatus() == 200) {
+            String responseJsonData = response.readEntity(String.class);
+            // work with dynamic list is better than static table
+            List<Country> countriesList = Arrays.asList(mapper.readValue(responseJsonData, Country[].class));
+
+            if(!countriesList.isEmpty()){
+                String capitalName = countriesList.get(0).getCapital();
+                System.out.println("capital Name is : " +capitalName);
+            }else{
+                System.out.println("transaction successfully completed without data returned back!");
+            }
+
+        } else {
+            String responseJsonData = response.readEntity(String.class);
+            Message message = mapper.readValue(responseJsonData, Message.class);
+            System.out.println("No Capital returned back: " +"Status code: "+ message.getStatus()+ " and " +"Error message: "+message.getMessage());
+        }
     }
 }
 
